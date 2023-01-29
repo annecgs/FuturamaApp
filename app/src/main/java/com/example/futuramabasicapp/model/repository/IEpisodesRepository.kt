6f84663ba4147f5b3e1fd3e9b3f3e278e8c26465
Ana Claudia Gomes Souza <103140224+annecgs@users.noreply.github.com>
2023-01-29 11:38:33 -0300

    Delete IEpisodesRepository.kt

--- app/src/main/java/com/example/futuramabasicapp/model/repository/IEpisodesRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.futuramabasicapp.model.repository
-
-import com.example.futuramabasicapp.model.dto.episodes.FuturamaEpisodes
-
-interface IEpisodesRepository {
-    suspend fun getEpisodes(): List<FuturamaEpisodes>
-}
