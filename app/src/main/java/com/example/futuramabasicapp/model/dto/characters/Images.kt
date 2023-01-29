01ba9824ef96f6da6ce68ffe405853ef5e2de130
Ana Claudia Gomes Souza <103140224+annecgs@users.noreply.github.com>
2023-01-29 11:39:46 -0300

    Delete Images.kt

--- app/src/main/java/com/example/futuramabasicapp/model/dto/characters/Images.kt
+++
@@ -1,9 +0,0 @@
-package com.example.futuramabasicapp.model.dto.characters
-
-import com.google.gson.annotations.SerializedName
-
-data class Images(
-    @SerializedName("head-shot")
-    val head_shot: String,
-    val main: String
-)
