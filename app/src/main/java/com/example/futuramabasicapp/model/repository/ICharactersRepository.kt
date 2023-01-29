ee7650f961b4c89585b8016d23bbf4855d13e838
Ana Claudia Gomes Souza <103140224+annecgs@users.noreply.github.com>
2023-01-29 11:38:45 -0300

    Delete ICharactersRepository.kt

--- app/src/main/java/com/example/futuramabasicapp/model/repository/ICharactersRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.futuramabasicapp.model.repository
-
-import com.example.futuramabasicapp.model.dto.characters.FuturamaCharacters
-
-interface ICharactersRepository {
-    suspend fun getCharacters(): List<FuturamaCharacters>
-}
